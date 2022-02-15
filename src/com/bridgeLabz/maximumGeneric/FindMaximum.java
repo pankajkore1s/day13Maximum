4a56da55e44df84253d97ce19e09f9daf87b864b
pankaj kore <korep95@gmail.com>
2022-02-15 15:03:19 +0530

    [pankaj]refactored the code for all in one generic and extended compare method

--- src/com/bridgeLabz/maximumGeneric/FindMaximum.java
+++ src/com/bridgeLabz/maximumGeneric/FindMaximum.java
@@ -4,13 +4,13 @@ public class FindMaximum {
 	public static void main(String[] args)  {
 		Operations operation = new Operations();
 		operation.getInputForInteger();
-		operation.testMaximumInteger(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
-		System.out.println(operation.maxInt+" is maximum amongst integer numbers");
+		int maxInt=operation.testMaximum(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
+		System.out.println(maxInt+" is maximum amongst integer numbers");
 		operation.getInputForFloat();
-		operation.testMaximumFloat(operation.number1,operation.number2,operation.number3);
-		System.out.println(operation.maxFloat+" is maximum amongst float numbers");
+		float maxFloat=operation.testMaximum(operation.number1,operation.number2,operation.number3);
+		System.out.println(maxFloat+" is maximum amongst float numbers");
 		operation.getInputForString();
-		operation.testMaximumString(operation.string1,operation.string2,operation.string3);
-		System.out.println(operation.maxString+" is maximum amongst words");
+		String maxString=operation.testMaximum(operation.string1,operation.string2,operation.string3);
+		System.out.println(maxString+" is maximum amongst words");
 	}
 }
--- src/com/bridgeLabz/maximumGeneric/Operations.java
+++ src/com/bridgeLabz/maximumGeneric/Operations.java
@@ -1,6 +1,6 @@
 package com.bridgeLabz.maximumGeneric;
 import java.util.*;
-public class Operations {
+public class Operations<E> {
 	public static Integer firstNumber,secondNumber,thirdNumber,maxInt;
 	public static Float number1=0.0f,number2=0.0f,number3=0.0f,maxFloat=0.0f;
 	public static String string1,string2,string3,maxString;
@@ -22,7 +22,7 @@ public class Operations {
 		System.out.println("Enter second float Number");
 		number2 = sc.nextFloat();
 		System.out.println("Enter third float Number");
-		number1 = sc.nextFloat();
+		number3 = sc.nextFloat();
 	}
 	public static void getInputForString() {
 		System.out.println("ENter First Word");
@@ -32,35 +32,14 @@ public class Operations {
 		System.out.println("ENter third Word");
 		string3=sc.next();
 	}
-	public static Integer testMaximumInteger(Integer x,Integer y,Integer z) {
-		maxInt = x;
-		if(y.compareTo(maxInt) > 0){
-			maxInt = y;
+	public static <E extends Comparable<E>> E testMaximum(E a,E b,E c) {
+		E max = a;
+		if(b.compareTo(max) > 0){
+			max = b;
 		}
-		if(z.compareTo(maxInt) > 0){
-			maxInt = z;
+		if(c.compareTo(max) > 0){
+			max = c;
 		}
-		return maxInt;
-	}
-	public static Float testMaximumFloat(Float a,Float b,Float c) {
-		maxFloat = a;
-		if(b.compareTo(maxFloat) > 0){
-			maxFloat = b;
+		return max;
 		}
-		if(c.compareTo(maxFloat) > 0){
-			maxFloat = c;
-		}
-		return maxFloat;
-	}
-	public static String testMaximumString(String p,String q,String r) {
-		maxString=p;
-		if(q.compareTo(maxString)>0) {
-			maxString=q;
-		}
-		if(r.compareTo(maxString)>0) {
-			maxString=r;
-		}
-		return maxString;
-	}
-
 }
